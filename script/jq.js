04c0eae366011e3dc24567ff8874d8cd05a950d6
amir jalili <jalilia370@gmail.com>
2017-08-22 16:36:31 +0430

    some basics are done

--- script/jq.js
+++ script/jq.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     "use strict";
-    $("body").
+    $("#menu-button").click(function(){
+        $("#menu-panel").slideToggle('medium',function(){
+            if($(this).is(":visible"))
+               $(this).css("display","flex");
+        });
+    })
 });
