8b59e215ecef07c8f7970e03fe4697f18c6b133f
amir jalili <jalilia370@gmail.com>
2017-08-24 10:56:16 +0430

    carousel added

--- script/jq.js
+++ script/jq.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     "use strict";
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items:1, loop:true, mouseDrag:false, nav:true, autoplay:true, autoplayTimeout:3000,
+        autoplayHoverPause:true, autoplaySpeed: 650, dotsSpeed:650, navSpeed:650
+    });
+    $("footer").css("top",($("body").height-$("footer").height)+"px");
     $("#menu-button").click(function(){
         $("#menu-panel").slideToggle('medium',function(){
             if($(this).is(":visible"))
